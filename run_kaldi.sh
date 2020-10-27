cd2b7c2a3e7d0aa2ead8d1e6d051a616a31a3d38
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2020-10-27 12:16:34 +0700

    fix run_kaldi script

--- run_kaldi.sh
+++ run_kaldi.sh
@@ -3,10 +3,8 @@ docker build -t chompk/kaldi docker;
 # run docker
 docker run -it \
 -v "$PWD/../../dataset/gowajee/:/mnt/gowajee" \
--v "$PWD/../../dataset/lotus/:/mnt/lotus" \
 -v "$PWD/gowajee/:/opt/kaldi/egs/gowajee" \
 -e KALDI_ROOT="/opt/kaldi" \
 -e GOWAJEE_ROOT="/mnt/gowajee" \
--e LOTUS_ROOT="/mnt/lotus" \
 -w "/opt/kaldi/egs/gowajee/s5" \
 chompk/kaldi:latest bash;
