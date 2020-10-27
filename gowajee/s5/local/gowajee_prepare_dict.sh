0e01eaae0276c320ab46d9b45bcc906639d73f3b
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2020-10-27 17:19:53 +0700

    add local scripts

--- gowajee/s5/local/gowajee_prepare_dict.sh
+++ gowajee/s5/local/gowajee_prepare_dict.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+# Modified from Voxforge
+# by Chompakorn Chaksangchaichot
 
 . path.sh || exit 1
 . utils/parse_options.sh || exit 1
@@ -8,3 +11,34 @@ if [ ! -f data/train/text ]; then
     exit 1
 fi
 
+locdata=data/local
+locdict=$locdata/dict
+mkdir -p $locdict
+g2p_model=g2p/model-5
+
+echo "--- Loading a Sequitur G2P model ..."
+
+if [ ! -f g2p/model-5 ]; then
+    echo "g2p model does not exists, creating one..."
+    # FIXME:
+    bash local/prep_g2p.sh
+fi
+
+echo "--- Prepare lexicon for vocabulary ..."
+cat $locdata/vocab-full.txt |\
+    sed -e 's:^\([^\s(]\+\)([0-9]\+)\(\s\+\)\(.*\):\1\2\3:' |\
+    egrep -v '<.?s>' > $locdict/vocab-plain.txt
+g2p.py --encoding UTF-8 --model=g2p/model-5 --apply $locdict/vocab-plain.txt |\
+    sort -u > $locdict/lexicon.txt
+
+echo "--- Prepare phone lists ..."
+echo SIL > $locdict/silence_phones.txt
+echo SIL > $locdict/optional_silence.txt
+grep -v -w sil data/local/dict/lexicon.txt |\
+    awk '{for(n=2;n<=NF;n++) { p[$n]=1; }} END{for(x in p) {print x}}' |\
+    sort > $locdict/nonsilence_phones.txt
+
+echo "--- Adding SIL to the lexicon ..."
+echo -e "!SIL\tSIL" >> $locdict/lexicon.txt
+
+echo "*** Dictionary preparation finished!"
--- gowajee/s5/run.sh
+++ gowajee/s5/run.sh
@@ -15,18 +15,11 @@
 # Make sure you have enough resources(CPUs and RAM) to accomodate this number of jobs
 njobs=2
 
-# The number of randomly selected speakers to be put in the test set
-nspk_test=20
-
-# Test-time language model order
+# language model order
 lm_order=2
 
 # Word position dependent phones?
 pos_dep_phones=true
-
-# The directory below will be used to link to a subset of the user directories
-# based on various criteria(currently just speaker's accent)
-selected=${DATA_ROOT}/selected
 
 # The user of this script could change some of the above parameters. Example:
 # /bin/bash run.sh --pos-dep-phones false
@@ -34,20 +27,19 @@ selected=${DATA_ROOT}/selected
 
 [[ $# -ge 1 ]] && { echo "Unexpected arguments"; exit 1; }
 
-# TODO:
 # Prepare ARPA LM and vocabulary using SRILM
-# local/gowajee_prepare_lm.sh --order ${lm_order} || exit 1
+local/gowajee_prepare_lm.sh --order ${lm_order} || exit 1
 
 # Prepare the lexicon and various phone lists
 # Pronunciations for OOV words are obtained using a pre-trained Sequitur model
-# local/gowajee_prepare_dict.sh || exit 1;
+local/gowajee_prepare_dict.sh || exit 1;
 
 # Prepare data/lang and data/local/lang directories
-# utils/prepare_lang.sh --position-dependent-phones $pos_dep_phones \
-#    data/local/dict '!SIL' data/local/lang data/lang || exit 1
+utils/prepare_lang.sh --position-dependent-phones $pos_dep_phones \
+   data/local/dict '!SIL' data/local/lang data/lang || exit 1
 
-# Prepare G.fst and data/{train,test} directories
-# local/gowajee_format_data.sh || exit 1
+# Prepare G.fst and data/{train,dev} directories
+local/gowajee_format_data.sh || exit 1
 
 # Now make MFCC features.
 mfccdir=${DATA_ROOT}/mfcc
@@ -78,7 +70,7 @@ steps/train_deltas.sh --cmd "$train_cmd" \
     data/train data/lang exp/mono exp/mono_ali || exit 1;
 
 # decode tri1
-utils/mkgraph.sh data/lang-test exp/tri1/graph || exit 1;
+utils/mkgraph.sh data/lang exp/tri1/graph || exit 1;
 steps/decode.sh --config conf/decode.config --nj $njobs --cmd "$decode_cmd" \
     exp/tri1/graph data/dev exp/tri1/decode
 
@@ -96,15 +88,98 @@ steps/train_deltas.sh --cmd "$train_cmd" 2000 11000 \
 # again, we use same lang for both train/dev
 utils/mkgraph.sh data/lang exp/tri2a exp/tri2a/graph
 steps/decode.sh --config conf/decode.config --nj $njobs --cmd "$decode_cmd" \
-    exp/tri2a/graph data/test exp/tri2a/decode
+    exp/tri2a/graph data/dev exp/tri2a/decode
 
 # train and decode tri2b [LDA+MLLT]
 steps/train_lda_mllt.sh --cmd "$train_cmd" 2000 11000 \
     data/train data/lang exp/tri1_ali exp/tri2b || exit 1;
 utils/mkgraph.sh data/lang exp/tri2b exp/tri2b/graph
 steps/decode.sh --config conf/decode.config --nj $njobs --cmd "$decode_cmd" \
-    exp/tri2b/graph data/test exp/tri2b/decode
+    exp/tri2b/graph data/dev exp/tri2b/decode
 
 # Align all data with LDA+MLLT system (tri2b)
 steps/align_si.sh --nj $njobs --cmd "$train_cmd" --use-graphs true \
     data/train data/lang exp/tri2b exp/tri2b_ali || exit 1;
+
+# Do MMI on top of LDA+MLLT.
+steps/make_denlats.sh --nj $njobs --cmd "$train_cmd" \
+    data/train data/lang exp/tri2b exp/tri2b_denlats || exit 1;
+steps/train_mmi.sh data/train data/lang exp/tri2b_ali exp/tri2b_denlats exp/tri2b_mmi || exit 1;
+steps/decode.sh --config conf/decode.config --iter 4 --nj $njobs --cmd "$decode_cmd" \
+    exp/tri2b/graph data/dev exp/tri2b_mmi/decode_it4
+steps/decode.sh --config conf/decode.config --iter 3 --nj $njobs --cmd "$decode_cmd" \
+    exp/tri2b/graph data/dev exp/tri2b_mmi/decode_it3
+
+# Do the same with boosting.
+steps/train_mmi.sh --boost 0.05 data/train data/lang \
+    exp/tri2b_ali exp/tri2b_denlats exp/tri2b_mmi_b0.05 || exit 1;
+steps/decode.sh --config conf/decode.config --iter 4 --nj $njobs --cmd "$decode_cmd" \
+    exp/tri2b/graph data/dev exp/tri2b_mmi_b0.05/decode_it4 || exit 1;
+steps/decode.sh --config conf/decode.config --iter 3 --nj $njobs --cmd "$decode_cmd" \
+    exp/tri2b/graph data/dev exp/tri2b_mmi_b0.05/decode_it3 || exit 1;
+
+# Do MPE.
+steps/train_mpe.sh data/train data/lang exp/tri2b_ali exp/tri2b_denlats exp/tri2b_mpe || exit 1;
+steps/decode.sh --config conf/decode.config --iter 4 --nj $njobs --cmd "$decode_cmd" \
+    exp/tri2b/graph data/dev exp/tri2b_mpe/decode_it4 || exit 1;
+steps/decode.sh --config conf/decode.config --iter 3 --nj $njobs --cmd "$decode_cmd" \
+    exp/tri2b/graph data/dev exp/tri2b_mpe/decode_it3 || exit 1;
+
+## Do LDA+MLLT+SAT, and decode.
+steps/train_sat.sh 2000 11000 data/train data/lang exp/tri2b_ali exp/tri3b || exit 1;
+utils/mkgraph.sh data/lang exp/tri3b exp/tri3b/graph || exit 1;
+steps/decode_fmllr.sh --config conf/decode.config --nj $njobs --cmd "$decode_cmd" \
+    exp/tri3b/graph data/dev exp/tri3b/decode || exit 1;
+
+# Align all data with LDA+MLLT+SAT system (tri3b)
+steps/align_fmllr.sh --nj $njobs --cmd "$train_cmd" --use-graphs true \
+    data/train data/lang exp/tri3b exp/tri3b_ali || exit 1;
+
+## MMI on top of tri3b (i.e. LDA+MLLT+SAT+MMI)
+steps/make_denlats.sh --config conf/decode.config \
+    --nj $njobs --cmd "$train_cmd" --transform-dir exp/tri3b_ali \
+    data/train data/lang exp/tri3b exp/tri3b_denlats || exit 1;
+steps/train_mmi.sh data/train data/lang exp/tri3b_ali exp/tri3b_denlats exp/tri3b_mmi || exit 1;
+
+steps/decode_fmllr.sh --config conf/decode.config --nj $njobs --cmd "$decode_cmd" \
+    --alignment-model exp/tri3b/final.alimdl --adapt-model exp/tri3b/final.mdl \
+    exp/tri3b/graph data/dev exp/tri3b_mmi/decode || exit 1;
+
+# Do a decoding that uses the exp/tri3b/decode directory to get transforms from.
+steps/decode.sh --config conf/decode.config --nj $njobs --cmd "$decode_cmd" \
+    --transform-dir exp/tri3b/decode  exp/tri3b/graph data/dev exp/tri3b_mmi/decode2 || exit 1;
+
+#first, train UBM for fMMI experiments.
+steps/train_diag_ubm.sh --silence-weight 0.5 --nj $njobs --cmd "$train_cmd" \
+    250 data/train data/lang exp/tri3b_ali exp/dubm3b
+
+# Next, various fMMI+MMI configurations.
+steps/train_mmi_fmmi.sh --learning-rate 0.0025 \
+    --boost 0.1 --cmd "$train_cmd" data/train data/lang exp/tri3b_ali exp/dubm3b exp/tri3b_denlats \
+    exp/tri3b_fmmi_b || exit 1;
+
+for iter in 3 4 5 6 7 8; do
+    steps/decode_fmmi.sh --nj $njobs --config conf/decode.config --cmd "$decode_cmd" --iter $iter \
+        --transform-dir exp/tri3b/decode  exp/tri3b/graph data/dev exp/tri3b_fmmi_b/decode_it$iter &
+done
+
+steps/train_mmi_fmmi.sh --learning-rate 0.001 \
+    --boost 0.1 --cmd "$train_cmd" data/train data/lang exp/tri3b_ali exp/dubm3b exp/tri3b_denlats \
+    exp/tri3b_fmmi_c || exit 1;
+
+for iter in 3 4 5 6 7 8; do
+    steps/decode_fmmi.sh --nj $njobs --config conf/decode.config --cmd "$decode_cmd" --iter $iter \
+        --transform-dir exp/tri3b/decode  exp/tri3b/graph data/dev exp/tri3b_fmmi_c/decode_it$iter &
+done
+
+# for indirect one, use twice the learning rate.
+steps/train_mmi_fmmi_indirect.sh --learning-rate 0.002 --schedule "fmmi fmmi fmmi fmmi mmi mmi mmi mmi" \
+    --boost 0.1 --cmd "$train_cmd" data/train data/lang exp/tri3b_ali exp/dubm3b exp/tri3b_denlats \
+    exp/tri3b_fmmi_d || exit 1;
+
+for iter in 3 4 5 6 7 8; do
+    steps/decode_fmmi.sh --nj $njobs --config conf/decode.config --cmd "$decode_cmd" --iter $iter \
+        --transform-dir exp/tri3b/decode  exp/tri3b/graph data/dev exp/tri3b_fmmi_d/decode_it$iter &
+done
+
+local/run_sgmm2.sh --nj $njobs
