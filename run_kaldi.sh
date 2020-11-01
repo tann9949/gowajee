f8dbfb457f9c9886ce37577efd49b7b691a00604
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2020-11-01 20:53:39 +0700

    add --device

--- run_kaldi.sh
+++ run_kaldi.sh
@@ -1,10 +1,12 @@
 # build docker
-docker build -t chompk/kaldi docker;
+docker build -t chompk/kaldi docker || exit 1;
 # run docker
-docker run -it \
--v "$PWD/../../dataset/gowajee/:/mnt/gowajee" \
+docker run -it --rm \
+-v "$PWD/../../../dataset/gowajee/:/mnt/gowajee" \
 -v "$PWD/gowajee/:/opt/kaldi/egs/gowajee" \
 -e KALDI_ROOT="/opt/kaldi" \
 -e GOWAJEE_ROOT="/mnt/gowajee" \
 -w "/opt/kaldi/egs/gowajee/s5" \
+--device /dev/snd:/dev/snd \
+--privileged \
 chompk/kaldi:latest bash;
