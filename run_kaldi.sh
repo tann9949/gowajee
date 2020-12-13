604cf4d8c0e6112a06ced8fbe23d401910d65a87
Chompakorn Chaksangchaichot <chompakornchaksangchaichot@Chompakorns-MacBook-Pro.local>
2020-12-13 15:59:56 +0700

    change mount dir

--- run_kaldi.sh
+++ run_kaldi.sh
@@ -2,11 +2,7 @@
 docker build -t chompk/kaldi docker || exit 1;
 # run docker
 docker run -it --rm \
--v "$PWD/../../../dataset/gowajee/:/mnt/gowajee" \
 -v "$PWD/gowajee/:/opt/kaldi/egs/gowajee" \
 -e KALDI_ROOT="/opt/kaldi" \
--e GOWAJEE_ROOT="/mnt/gowajee" \
 -w "/opt/kaldi/egs/gowajee/s5" \
--e PULSE_SERVER=host.docker.internal \
--v /Users/chompk/.config/pulse:/home/pulseaudio/.config/pulse \
 chompk/kaldi:latest bash;
